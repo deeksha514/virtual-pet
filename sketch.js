41f24a134cd4049049a554ac876a4abff9ae1e08
deeksha514 <65813290+deeksha514@users.noreply.github.com>
2020-09-04 16:46:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ var dogIMG,happydog;
 function preload()
 {
   //load images here
-  dogIMG=loadImage("images/dogImg.png");
-  happydog=loadImage("images/dogImg1.png");
+  dogIMG=loadImage("Dog.png");
+  happydog=loadImage("happydog.png");
   
 }
 
@@ -13,21 +13,20 @@ function setup() {
   createCanvas(500,500);
 
   database=firebase.database();
-  foodStock=database.ref('Food');
+  foodStock=database.ref('food');
   foodStock.on("value",readStock);
  
   dog=createSprite(250,450,10,10);
+  dog.addImage(dogIMG);
+  dog.scale=0.3;
  
 }
 
 
 function draw() {  
   background(46,139,87);
- 
-  dog.addImage(dogIMG);
-  dog.scale=0.1;
 
-  if(keyWentDown===UP_ARROW){
+  if(keyWentDown(UP_ARROW)){
     writeStock(foodS);
     dog.addImage(happydog);
   }
@@ -38,20 +37,20 @@ function draw() {
 text("press up arrow key to feed ",20,50);
 }
 
-function writeStock(x){
-  database.ref('/').update({
-    food:x
-})
+function readStock(data){
+  foodS=data.val();
 }
 
 
-function readStock(data){
+function writeStock(x){
   if(x<=0){
     x=0
   }else{
     x=x-1
   }
-  foodS=data.val();
+  database.ref('/').update({
+    food:x
+  })
 }
 
 
